a49fb9875564a970e481d789f879923b4892eee1
Ryan Attard <ryanattard@gmail.com>
2022-04-12 08:55:52 +0200

    Role allocation by default to every newly registered user

--- Presentation/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Presentation/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -83,30 +83,41 @@ namespace Presentation.Areas.Identity.Pages.Account
                     FirstName=Input.FirstName, LastName=Input.LastName };
 
                 var result = await _userManager.CreateAsync(user, Input.Password);
+
+                //allocation of a default role
+             
+
                 if (result.Succeeded)
-                {
-                    _logger.LogInformation("User created a new account with password.");
-
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                {   
+                    
+                   result = await _userManager.AddToRoleAsync(user, "User");
+                    if(result.Succeeded)
                     {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                             _logger.LogInformation("User created a new account with password.");
+
+                                                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                                                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                                                var callbackUrl = Url.Page(
+                                                    "/Account/ConfirmEmail",
+                                                    pageHandler: null,
+                                                    values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                                                    protocol: Request.Scheme);
+
+                                                await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                                                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                                                if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                                                {
+                                                    return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                                                }
+                                                else
+                                                {
+                                                    await _signInManager.SignInAsync(user, isPersistent: false);
+                                                    return LocalRedirect(returnUrl);
+                                                }
                     }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
+                    
+                   
                 }
                 foreach (var error in result.Errors)
                 {
